93ca94c4dec2789e5fe481f3b5188f44f8fb9e5f
KrzysztofSym <krzysztof.symoniuk@gmail.com>
2020-12-15 09:02:45 +0100

    finish styling descriptions on page 1

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 
 const introduction1Height = document.querySelector('.introduction1').clientHeight
-const introduction1DistanceTop = document.querySelector('.introduction1').offsetHeight
-const pic = document.querySelector('div.bg')
+const introduction1DistanceTop = document.querySelector('.introduction1').offsetTop
+const pic1 = document.querySelector('div.bg1')
+const paragraph1 = document.querySelector('.introduction1 span p')
+const hr1 = document.querySelector('.introduction1 span hr')
+const introduction2Height = document.querySelector('.introduction2').clientHeight
+const introduction2DistanceTop = document.querySelector('.introduction2').offsetTop
+const pic2 = document.querySelector('div.bg2')
+const paragraph2 = document.querySelector('.introduction2 span p')
+const hr2 = document.querySelector('.introduction2 span hr')
+
+
+// const distanceToTop = pic.getBoundingClientRect().top;
 
 // console.log(introduction1DistanceTop);
 function imgSlide() {
     const scrollSize = window.scrollY
     //console.log(scrollSize);
-    if (scrollSize > (introduction1DistanceTop + introduction1Height * 0.2)) {
-        pic.classList.add('active')
-        console.log('działa :)');
-    }
-
-}
+    if ((scrollSize > (introduction1DistanceTop - window.innerHeight * 0.6)) && (pic1.classList.contains('active') !== true)) {
+        pic1.classList.add('activePic')
+        paragraph1.classList.add('activeP')
+        hr1.classList.add('activeHr')
+        
+        // console.log(introduction1DistanceTop);
+        // console.log(window.innerHeight);
+    };
+    if ((scrollSize > (introduction2DistanceTop - window.innerHeight * 0.6)) && (pic2.classList.contains('active') !== true)) {
+        pic2.classList.add('activePic')
+        paragraph2.classList.add('activeP')
+        hr2.classList.add('activeHr')
+        
+        console.log('yeah');
+}}
 window.addEventListener("scroll", imgSlide)

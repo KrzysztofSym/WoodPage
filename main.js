f91e951e453c014c93642bdf1bd6b88de0b29071
KrzysztofSym <krzysztof.sym@gmail.com>
2022-06-15 23:21:25 +0200

    add featured and half of JS

--- main.js
+++ main.js
@@ -50,3 +50,22 @@ function imgSlide() {
 }}
 window.addEventListener("scroll", imgSlide);
 
+// Cabinet slider
+let thumbnails = document.getElementsByClassName('thumbnail')
+
+		let activeImages = document.getElementsByClassName('active')
+
+		for (let i=0; i < thumbnails.length; i++){
+
+			thumbnails[i].addEventListener('mouseover', function(){
+				console.log(activeImages)
+
+				if (activeImages.length > 0){
+					activeImages[0].classList.remove('active')
+				}
+
+
+				this.classList.add('active')
+				document.getElementById('featured').src = this.src
+			})
+		}
